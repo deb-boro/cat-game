59fc4cb6b359bf4fcb50f43f97f7600dc60a2c98
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-08-04 19:17:10 -0400

    add incoming changes for user.js

--- seeds/users.js
+++ seeds/users.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-const { User } = require('../models');
-
-const userdata = [
-    {
-        username: 'bradley',
-        email: 'bradley@gmail.com',
-        password: '12345'
-    },
-    {
-        username: 'deb',
-        email: 'deb@gmail.com',
-        password: '67890',
-    },
-    {
-        username: 'andrew',
-        email: 'andrew@gmail.com',
-        password: '00000'
-    }
-]
-
-
-// const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
-const userfunction  = () => User.bulkCreate(userdata);
-
-module.exports = userfunction;
-=======
 const sequelize = require('../config/connection')
 const { User } = require('../models')
 
@@ -51,4 +24,3 @@ const seedUsers = () => User.bulkCreate(userdata, { individualHooks: true })
 //   User.bulkCreate(userdata)
 // }
 module.exports = seedUsers
->>>>>>> main
