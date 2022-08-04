8866a04acd995d3d9d9bfc5112abe8456b361690
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-08-04 19:41:53 -0400

    add get route for single cat by id

--- routes/api/cat-routes.js
+++ routes/api/cat-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const res = require('express/lib/response')
-const { Cat } = require('../../models')
+const { Cat, User } = require('../../models')
 
 // get all cats
 router.get('/', (req, res) => {
@@ -11,6 +11,37 @@ router.get('/', (req, res) => {
       res.status(500).json(err)
     })
 });
+
+router.get('/:id', (req, res) => {
+  Cat.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'name',
+      'color',
+      'user_id'
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbCatData => {
+    if (!dbCatData) {
+      res.status(404).json({ message: 'No cat found with this id' });
+      return;
+    }
+    res.json(dbCatData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+})
 
 router.post('/', (req, res) => {
   Cat.create({
