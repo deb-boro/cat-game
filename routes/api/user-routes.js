fd8820618075ee0ee67e29d0bf266cc5f86de915
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-08-04 20:21:38 -0400

    write route for get user by id

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const res = require('express/lib/response')
-const { User } = require('../../models')
+const { User, Cat } = require('../../models')
 const sequelize = require('../../config/connection')
 
 // get all users
@@ -13,6 +13,36 @@ router.get('/', (req, res) => {
       console.log(err)
       res.status(500).json(err)
     })
+})
+
+router.get('/:id', (req, res) => {
+  User.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'username',
+      'email'
+    ],
+    include: [
+      {
+        model: Cat,
+        attributes: [ 'id', 'name', 'color' ]
+      }
+    ]
+  })
+  .then(dbCatData => {
+    if (!dbCatData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbCatData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 })
 
 module.exports = router
