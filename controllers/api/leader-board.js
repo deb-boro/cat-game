ddab3545371d561f670cf470fb2f394ceef2494a
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-08-09 21:08:20 -0400

    add leader routes to index.js, write get route to find cats by order of click count, write initial handlebars for leaderboard

---
+++ controllers/api/leader-board.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const res = require('express/lib/response')
+const { User, Cat } = require('../../models')
+
+router.get('/', (req, res) => {
+    Cat.findAll({
+       order: 'click_count'
+    })
+    .then(dbLeaderData => res.json(dbLeaderData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
