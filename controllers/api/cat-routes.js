74653371cee7c4eb3ca488205ad419a4ed11c99e
deb boro <debajit.boro666@gmail.com>
2022-08-11 00:52:42 -0400

    updated clicks

--- controllers/api/cat-routes.js
+++ controllers/api/cat-routes.js
@@ -1,16 +1,21 @@
 const router = require('express').Router()
 const { Cat, Clicks, User } = require('../../models')
-const sequelize = require('../../config/connection');
+const sequelize = require('../../config/connection')
 // const { ValidationError } = require('sequelize/types');
 
 //get all cats
 router.get('/', (req, res) => {
   Cat.findAll({
     attributes: [
-      'id', 
-      'name', 
+      'id',
+      'name',
       'color',
-      [sequelize.literal('(SELECT COUNT(*) FROM clicks WHERE cat.id = clicks.cat_id)'), 'click_count']
+      [
+        sequelize.literal(
+          '(SELECT COUNT(*) FROM clicks WHERE cat.id = clicks.cat_id)',
+        ),
+        'click_count',
+      ],
     ],
     include: [
       {
@@ -34,10 +39,15 @@ router.get('/:id', (req, res) => {
       id: req.params.id,
     },
     attributes: [
-      'id', 
-      'name', 
+      'id',
+      'name',
       'color',
-      [sequelize.literal('(SELECT COUNT(*) FROM clicks WHERE cat.id = clicks.cat_id)'), 'click_count']
+      [
+        sequelize.literal(
+          '(SELECT COUNT(*) FROM clicks WHERE cat.id = clicks.cat_id)',
+        ),
+        'click_count',
+      ],
     ],
     include: [
       {
@@ -46,23 +56,20 @@ router.get('/:id', (req, res) => {
       },
     ],
   })
-  .then((dbCatData) => {
-    if (!dbCatData) {
-      res.status(404).json({ message: 'No cat found with this id' })
-      return;
-    }
-    res.json(dbCatData);
-
-    res.render('training', {
-      text: 'CAT click will happen here',
-      loggedIn: req.session.loggedIn,
+    .then((dbCatData) => {
+      if (!dbCatData) {
+        res.status(404).json({ message: 'No cat found with this id' })
+        return
+      }
+      res.render('training', {
+        loggedIn: req.session.loggedIn,
+      })
     })
-  })
-  .catch((err) => {
-    console.log(err)
-    res.status(500).json(err)
-  })
-});
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+})
 
 router.post('/', (req, res) => {
   Cat.create({
@@ -82,52 +89,58 @@ router.post('/', (req, res) => {
 router.put('/clicks', (req, res) => {
   Clicks.create({
     user_id: req.body.user_id,
-    cat_id: req.body.cat_id
-  }).then(() => {
-    return Cat.findOne({
-      where: {
-        id: req.body.cat_id
-      },
-      attributes: [
-        'id',
-        'name',
-        'color',
-        'user_id',
-        [sequelize.literal('(SELECT COUNT(*) FROM clicks WHERE cat.id = clicks.cat_id)'), 'click_count']
-      ]
+    cat_id: req.body.cat_id,
+  })
+    .then(() => {
+      return Cat.findOne({
+        where: {
+          id: req.body.cat_id,
+        },
+        attributes: [
+          'id',
+          'name',
+          'color',
+          'user_id',
+          [
+            sequelize.literal(
+              '(SELECT COUNT(*) FROM clicks WHERE cat.id = clicks.cat_id)',
+            ),
+            'click_count',
+          ],
+        ],
+      })
     })
-  })
-  .then(dbCatData => res.json(dbCatData))
-  .catch(err => {
-    console.log(err);
-    res.status(400).json(err);
-  })
-});
+    .then((dbCatData) => res.json(dbCatData))
+    .catch((err) => {
+      console.log(err)
+      res.status(400).json(err)
+    })
+})
 
 router.put('/:id', (req, res) => {
   Cat.update(
     {
       name: req.body.name,
-      color: req.body.color
+      color: req.body.color,
     },
     {
       where: {
-        id: req.params.id
-      }
-    }
+        id: req.params.id,
+      },
+    },
   )
-    .then(dbPostData => {
+    .then((dbPostData) => {
       if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
+        res.status(404).json({ message: 'No post found with this id' })
+        return
       }
-      res.json(dbPostData);
+      res.json(dbPostData)
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+})
 
 router.delete('/:id', (req, res) => {
   Cat.destroy({
@@ -147,6 +160,5 @@ router.delete('/:id', (req, res) => {
       res.status(500).json(err)
     })
 })
-
 
 module.exports = router
